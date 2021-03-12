55d323f98fb12dc734ad5e0ba1e915d3aede1b7e
Viktor Gubarevich <gubarew.v@gmail.com>
2021-03-12 22:54:47 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    const arr = [];
+    const map = new Map(bracketsConfig);
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === map.get(arr[arr.length - 1])) {
+            arr.pop();
+        } else arr.push(str[i]);
+    }
+
+    return arr.length === 0;
 }
